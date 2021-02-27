83fc0d909ca37c7b4a17b233eeadb26e56fdd1b5
itssunexploding <its_sun_exploding@mail.ru>
2021-02-27 22:06:34 +0300

    Add some code formating

--- task_1.sh
+++ task_1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 read fileName 
-filesDir=$(find /home "$PWD" -type f)
+filesDir=$(find $fileName /home "$PWD" -type f)
 filePresence=0
 IFS=$'\n'
 for var in $filesDir
-do
-if [[ "$var" == *"$fileName"* ]]
-then
-echo $var
-echo $(cat $var)
-filePresence=1
-fi
-done
+    do
+        if [[ "$var" == *"$fileName"* ]]
+            then
+                #echo $var
+                echo $(cat $var)
+                filePresence=1
+        fi
+    done
 if [ $filePresence -eq 0 ]
-then
-echo "$fileName" | cut -c 5-
+    then
+        echo "$fileName" | cut -c 5-
 fi
